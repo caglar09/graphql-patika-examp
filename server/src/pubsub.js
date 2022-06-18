dc4a1e728676f5ac82257165577f681e0192acb5
Caglar YILDIRIM <cglr.1998@hotmail.com>
2022-06-18 13:07:00 +0300

    console removed from server

--- server/src/pubsub.js
+++ server/src/pubsub.js
@@ -3,7 +3,7 @@ import Redis from "ioredis";
 
 import dotenv from "dotenv";
 dotenv.config()
-console.log(process.env);
+
 const options = {
   host: process.env.REDIS_HOST,
   port: process.env.REDIS_PORT,
