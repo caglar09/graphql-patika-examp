8de6e4de8fce8d5d0e3c737da8b3f84075b1ec92
Caglar YILDIRIM <cglr.1998@hotmail.com>
2023-01-07 09:22:26 +0300

    ws added

--- client/src/apollo.js
+++ client/src/apollo.js
@@ -1,13 +1,34 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from "@apollo/client";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { GraphQLWsLink } from "@apollo/client/link/subscriptions";
+import { createClient } from "graphql-ws";
+
+const wsLink = new GraphQLWsLink(
+  createClient({
+    url: "ws://localhost:4000",
+    connectionParams: {
+      authToken: "asdad",
+    },
+  })
+);
+
+const httpLink = new HttpLink({
+  uri: "http://localhost:4000",
+});
+
+const splitLink = split(
+  ({ query }) => {
+    const definition = getMainDefinition(query);
+    return (
+      definition.kind === "OperationDefinition" &&
+      definition.operation === "subscription"
+    );
+  },
+  wsLink,
+  httpLink
+);
 
 const client = new ApolloClient({
-  uri: "http://localhost:4000",
+  link: splitLink,
   cache: new InMemoryCache(),
 });
 
